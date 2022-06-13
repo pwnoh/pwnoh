6f06378837664f55b5e0ffe0fea6d216958b8c04
pwccaa <mattos1@mx2.unisc.br>
2022-06-13 00:01:31 -0300

    Changed input and button font

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function validate(){
   let username = document.getElementById("user").value
   let password = document.getElementById("passwd").value
 
-  if(username == "pwcca" && password == "123") {
+  if (username == "pwcca" && password == "123") {
       window.location='https://github.com/pwccaa'
+  } else if (username == "ynohh" && password == "123") {
+    window.location='https://github.com/ynohh'
   } else {
       window.alert("Invalid user.")
-  }
-  
-  }
+    }
+}
